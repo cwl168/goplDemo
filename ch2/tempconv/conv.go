e9d5929d3e57a0400dbbee0b5298a1a10deda624
caoweilin <1017548707@qq.com>
2020-09-02 21:17:54 +0800

    gopl

--- ch2/tempconv/conv.go
+++ ch2/tempconv/conv.go
@@ -8,9 +8,15 @@
 package tempconv
 
 // CToF converts a Celsius temperature to Fahrenheit.
-func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+func CToF(c Celsius) Fahrenheit {
+	//类型转化，非函数调用
+	return Fahrenheit(c*9/5 + 32)
+}
 
 // FToC converts a Fahrenheit temperature to Celsius.
-func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+func FToC(f Fahrenheit) Celsius {
+	//类型转化，非函数调用
+	return Celsius((f - 32) * 5 / 9)
+}
 
 //!-
